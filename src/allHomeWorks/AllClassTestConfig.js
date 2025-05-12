9d95c6e39c4bd70d033e74aacdf3fc3a6a9bf283
weiliams596 <152355802+weiliams596@users.noreply.github.com>
2025-05-12 14:50:08 +0500

    Update AllClassTestConfig.js

--- src/allHomeWorks/AllClassTestConfig.js
+++ src/allHomeWorks/AllClassTestConfig.js
@@ -7,7 +7,7 @@ import ClassTest1_4 from './Week26/D4_W26_HW/ClassTest/ClassTest';
 // Week 27
 import ClassTest2_1 from './Week27/D1_W27_HW/ClassTest/ClassTest';
 import ClassTest2_2 from './Week27/D2_W27_HW/ClassTest/ClassTest';
-import ClassTest2_3 from './Week27/D3_W27_HW/ClassTest/ClassTest';
+import ClassTest2_3 from './Week27/D3_W27_Hw/ClassTest/ClassTest';
 
 // Week 28
 // Router exercise
